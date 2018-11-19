6a35ddab99e78c9f950664eecb382a92eb111855
Nicholas <nicholaskappel@prezap.de>
2018-11-19 15:31:41 +0100

    remove duplicated file

--- facebook_api.py
+++
@@ -1,8 +0,0 @@
-import facebook 
-
-graph = facebook.GraphAPI(access_token="EAAFB8eOiQXsBAAygETK6tOcRI1fXKRlZBZAs3gEzvNrYOa1EH4l7NDYLG9RxhFpU9MUOhInyb2N0HxfO1flvACyIcm58ieOjhYJWmuv9XFFZC0aMjYbUw94YcG1bqjFt52qgFeKPYcxlywZAR9ly59ZBIWdrcElcaR87sg42XbgZDZD", version="3.1")
-
-post = graph.get_object(id='me', fields='id,name')
-
-print(post['id'] + ' ' + post['name'])
-
