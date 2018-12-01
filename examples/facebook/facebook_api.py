b390ecb13c7ce4a70846870657343ed00562c151
Dominik Möllers <d_moel07@uni-muenster.de>
2018-12-01 17:39:51 +0100

    Natural Language Processing Example and extended Facebook_api script (should be finished)

--- examples/facebook/facebook_api.py
+++ examples/facebook/facebook_api.py
@@ -1,12 +1,40 @@
 import facebook
+import json
 
 
-graph = facebook.GraphAPI(access_token='mytoken', version='3.1')
+graph = facebook.GraphAPI(access_token='access_token', version='3.1')
 
-post = graph.get_object(id='me', fields='id,name')
+#personal information
+personal = graph.get_object(id='me', fields='id,name,address,hometown,age_range,gender')
 
-print(post['id'] + ' ' + post['name'])
+#writing personal data into json-File
+with open('personal.json', 'w') as outfile: 
+	json.dump(personal, outfile, ensure_ascii=False)
 
-# print likes of the user related to the access_token
-likes = graph.get_connections(id='me', connection_name='likes')
-print(likes)
+#likes
+likes = graph.get_connections(id='me', connection_name='likes', fields='name,category,about,genre')
+	
+#writing like data into json-File
+with open('likes.json', 'w') as outfile: 
+	json.dump(likes, outfile, ensure_ascii=False)
+	
+#posts
+posts = graph.get_connections(id='me', connection_name='posts', fields='message,place,message_tags')
+
+#writing post data into json-File	
+with open('posts.json', 'w') as outfile: 
+	json.dump(posts, outfile, ensure_ascii=False)	
+
+#events
+events = graph.get_connections(id='me', connection_name='events', fields='name,description,place,rsvp_status')
+
+#writing event data into json-File
+with open('events.json', 'w') as outfile: 
+	json.dump(events, outfile, ensure_ascii=False)
+
+#groups
+groups = graph.get_connections(id='me', connection_name='groups', fields='name,description')
+
+#writing group data into json-File 
+with open('groups.json', 'w') as outfile: 
+	json.dump(groups, outfile, ensure_ascii=False)
