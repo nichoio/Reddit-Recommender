65e9809b0bc86fd467ba3024182c43b20fa40a24
alexj2510 <alexanderjohnsdorf@web.de>
2018-12-01 16:13:57 +0100

    Add files via upload

--- examples/twitter/twitter_api.py
+++ examples/twitter/twitter_api.py
@@ -1,3 +1,4 @@
+
 import json
 import os
 
@@ -6,20 +7,22 @@ import twitter
 
 # call http://gettwitterid.com/ to get the id by username
 USER_ID = 123
+USER_NAME = "LinusTech"
 
-TWEET_AMOUNT = 200  # cannot extend 200
+TWEET_AMOUNT = 500  # cannot extend 200
 JSON_PATH = os.path.join('', '{}.json'.format(USER_ID))
 
 # call https://developer.twitter.com/en/apps/app_id to get the credentials
-api = twitter.Api(
-    consumer_key='consumer_key',
-    consumer_secret='consumer_secret',
-    access_token_key='access_token_key',
-    access_token_secret='access_token_secret')
+api = twitter.Api(consumer_key='consumer_key',
+                  consumer_secret='consumer_secret',
+                  access_token_key='token_key',
+                  access_token_secret='token_secret')
 
 # get latest 200 tweets by this user.
-data = api.GetUserTimeline(user_id=USER_ID, count=TWEET_AMOUNT)
+data = api.GetUserTimeline(screen_name=USER_NAME, count=TWEET_AMOUNT)
+data2  = api.GetUserTimeline(screen_name=USER_NAME, count=TWEET_AMOUNT, max_id=(data[199].id-1))
 as_json = [json.loads(str(d)) for d in data]
+as_json.extend([json.loads(str(d)) for d in data2])
 
 # write to file as JSON
 with open(JSON_PATH, 'w') as file:
