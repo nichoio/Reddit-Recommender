8f2eb3f749cd2ba16e01f63e5e9fcbfa9342a319
Dominik Möllers <d_moel07@uni-muenster.de>
2018-12-08 11:21:05 +0100

    Finished the facebook transformation + some json data

--- database_scripts/createschemaandtable_script.sql
+++ database_scripts/createschemaandtable_script.sql
@@ -8,41 +8,41 @@ CREATE SCHEMA IF NOT EXISTS reddit_recommender;
 #Creating the Personal Table   
 DROP TABLE IF EXISTS reddit_recommender.personal;
 CREATE TABLE reddit_recommender.personal(
-	u_id VARCHAR(20), 
+    u_id BIGINT, 
     name VARCHAR(50),
-    address VARCHAR(50),
-    hometown VARCHAR(50),
     age_range VARCHAR(10),
     gender VARCHAR(10));
 
 #Creating the Likes Table       
 DROP TABLE IF EXISTS reddit_recommender.likes;
 CREATE TABLE reddit_recommender.likes(
-    u_id VARCHAR(20),
+    u_id BIGINT,
     name VARCHAR(50),
     category VARCHAR(50),
-    about TEXT,
-    genre VARCHAR(50));
+    about LONGTEXT,
+    genre VARCHAR(100));
 
 #Creating the Posts Table       
 DROP TABLE IF EXISTS reddit_recommender.posts;
 CREATE TABLE reddit_recommender.posts(
-	u_id VARCHAR(20),
-    message TEXT, 
-    place VARCHAR(50),
-    message_tags TEXT);
+	u_id BIGINT,
+    message LONGTEXT, 
+    place VARCHAR(50));
 
 #Creating the Events Table       
 DROP TABLE IF EXISTS reddit_recommender.events;
 CREATE TABLE reddit_recommender.events(
-	u_id VARCHAR(20), 
+	u_id BIGINT, 
     name VARCHAR(50),
-    place VARCHAR(50),
+    place TINYTEXT,
+    city VARCHAR(50),
+    zip VARCHAR(20),
+    country VARCHAR(50),
     rsvp_status VARCHAR(20));
 
 #Creating the Groups Table    
 DROP TABLE IF EXISTS reddit_recommender.groups;
 CREATE TABLE reddit_recommender.groups(
-	u_id VARCHAR(20), 
+	u_id BIGINT, 
     name VARCHAR(50),
-    description TEXT);
+    description LONGTEXT);
