115d13009cc7bcb32b56f572ac5e6e2589291850
Nicholas K <nicholaskappel@prezap.de>
2019-01-14 23:15:53 +0100

    table creation for mysql

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ docker-compose -f docker/docker-compose.yml up -d
 sleep 20 # give mysql container time to spin up db
 docker exec -it docker_reddit-mysql_1 /bin/bash -c \
 'mysql -u root --password=password -e '\''CREATE USER "root"@"%" IDENTIFIED BY "password"; GRANT ALL PRIVILEGES ON *.* TO "root"@"%" WITH GRANT OPTION;'\'''
+docker exec -it docker_reddit-mysql_1 /bin/bash -c \
+'mysql -u root --password=password < /tmp/sqlkomplett.sql'
 
 echo "Done!"
