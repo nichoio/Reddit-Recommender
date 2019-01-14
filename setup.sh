767253c5afee9acb4afe3205300276be893dc517
Nicholas K <nicholaskappel@prezap.de>
2019-01-14 22:48:17 +0100

    adjusting user rights for mysql

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 docker-compose -f docker/docker-compose.yml build
-echo "DOOOONE"
+docker-compose -f docker/docker-compose.yml up -d
+sleep 20 # give mysql container time to spin up db
+docker exec -it docker_reddit-mysql_1 /bin/bash -c \
+'mysql -u root --password=password -e '\''CREATE USER "root"@"%" IDENTIFIED BY "password"; GRANT ALL PRIVILEGES ON *.* TO "root"@"%" WITH GRANT OPTION;'\'''
+
+echo "Done!"
