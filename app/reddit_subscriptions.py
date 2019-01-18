0e3faabee32420d8d1378d92a9f8537c2e1e6796
Nicholas K <nicholaskappel@prezap.de>
2019-01-19 00:05:04 +0100

    working oauth flow for reddit

---
+++ app/reddit_subscriptions.py
@@ -0,0 +1,65 @@
+import json
+
+import praw
+
+CLIENT_ID = 'hn7qi5bxIVgXBg'
+CLIENT_SECRET = 'dCVJ0MpJ2NzFugKN-G9BiYxBrok'
+REDIRECT_URI = 'http://localhost:5000/redirect-reddit'
+USER_AGENT = 'reddit-recommender:v1.2'
+JSON_PATH = 'reddit_user.json'
+
+
+def gen_praw_object():
+    return praw.Reddit(client_id=CLIENT_ID,
+                client_secret=CLIENT_SECRET,
+                user_agent=USER_AGENT,
+                redirect_uri=REDIRECT_URI)
+
+
+def group_by(list_):
+    dict_ = dict()
+    for l in list_:
+        if l in dict_:
+            dict_[l] += 1
+        else:
+            dict_[l] = 1
+    return dict_
+
+
+def save_subs(refresh_token):
+    reddit = praw.Reddit(client_id=CLIENT_ID,
+                         client_secret=CLIENT_SECRET,
+                         user_agent=USER_AGENT,
+                         refresh_token=refresh_token)
+
+    user = reddit.user.me()
+    subscribed = [s.display_name for s in list(reddit.user.subreddits(limit=None))]
+    subscribed_data = [{
+        'sub_name': s,
+        'user_name': user.name,
+        'subscribed': True}
+        for s in subscribed]
+
+    comments_subs = [c.subreddit.display_name for c in user.comments.new()]
+    comment_freq = group_by(comments_subs)
+
+    # get amount of comments for all subscribed subs
+    for s in subscribed_data:
+        if s['sub_name'] in comment_freq:
+            s['comments'] = comment_freq[s['sub_name']]
+        else:
+            s['comments'] = 0
+
+    # add subs with comments but no subscription
+    commented_no_sub = set(list(comment_freq.keys())) - set(subscribed)
+    commented_data = [{
+        'sub_name': c,
+        'user_name': user.name,
+        'subscribed': False,
+        'comments': comment_freq[c]}
+        for c in commented_no_sub]
+
+    subscribed_data.extend(commented_data)
+
+    with open(JSON_PATH, 'w') as outfile:
+        json.dump(subscribed_data, outfile)
