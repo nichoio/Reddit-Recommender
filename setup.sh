91e7d5444fd295da2e7db3f90cfcfdcf154c5662
Nicholas K <nicholaskappel@prezap.de>
2019-01-18 12:43:53 +0100

    initial flask setup + setup fix

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 docker-compose build
 docker-compose up -d
 sleep 20 # give mysql container time to spin up db
-docker exec -it docker_reddit-mysql_1 /bin/bash -c \
+docker exec -it reddit-recommender_reddit-mysql_1 /bin/bash -c \
 'mysql -u root --password=password -e '\''CREATE USER "root"@"%" IDENTIFIED BY "password"; GRANT ALL PRIVILEGES ON *.* TO "root"@"%" WITH GRANT OPTION;'\'''
-docker exec -it docker_reddit-mysql_1 /bin/bash -c \
+docker exec -it reddit-recommender_reddit-mysql_1 /bin/bash -c \
 'mysql -u root --password=password < /tmp/sqlkomplett.sql'
 
 echo "Done!"
