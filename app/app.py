9051269fb083935e63a4d90ab7b258abbd4bb74e
Nicholas K <nicholaskappel@prezap.de>
2019-01-18 13:32:59 +0100

    dependencies for webserver

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def start(name=None):
+    return render_template('start.html', name=name)
 
 #if __name__ == '__main__':
 #	app.run(host='0.0.0.0')
