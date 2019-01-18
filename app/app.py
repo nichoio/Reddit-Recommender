c36835d1b7b3c3e86f417c2f875f2c68fd1d0b7e
Nicholas K <nicholaskappel@prezap.de>
2019-01-18 17:06:45 +0100

    mysql connection for flask

--- app/app.py
+++ app/app.py
@@ -1,10 +1,18 @@
+import sqlalchemy
 from flask import Flask, render_template
 
+DB_PATH = 'mysql+pymysql://root:password@reddit-mysql/reddit_recommender'
+
 app = Flask(__name__)
+db = sqlalchemy.create_engine(DB_PATH)
+
 
 @app.route('/')
 def start(name=None):
     return render_template('start.html', name=name)
 
-#if __name__ == '__main__':
-#	app.run(host='0.0.0.0')
+@app.route('/db-test')
+def db_test():
+	with db.engine.connect() as con:
+		result = con.execute('SELECT COUNT(*) AS count FROM subreddits;')
+	return str(result.fetchone()['count'])
