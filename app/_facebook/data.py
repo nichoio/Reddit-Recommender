bc0d6b0b1e1dab612ab1f1a9e6aa079f4a828a69
Dominik Möllers <d_moel07@uni-muenster.de>
2019-01-24 16:27:29 +0100

    Modified paths in jobs, transformation and script

--- app/_facebook/data.py
+++ app/_facebook/data.py
@@ -14,33 +14,33 @@ graph = facebook.GraphAPI(access_token='', version='3.1')
 personal = graph.get_object(id='me', fields='id,name,gender')
 
 #writing personal data into json-File
-with open('personal.json', 'w', encoding='utf-8') as outfile: 
+with open('/tmp/fb_personal.json', 'w', encoding='utf-8') as outfile:
 	json.dump(personal, outfile, ensure_ascii=False, indent=3)
 
 #likes
 likes = graph.get_connections(id='me', connection_name='likes', fields='name,category,about,genre')
 	
 #writing like data into json-File
-with open('likes.json', 'w', encoding='utf-8') as outfile: 
+with open('/tmp/fb_likes.json', 'w', encoding='utf-8') as outfile:
 	json.dump(likes, outfile, ensure_ascii=False, indent=3)
 	
 #posts
 posts = graph.get_connections(id='me', connection_name='posts', fields='message,place,message_tags')
 
 #writing post data into json-File	
-with open('posts.json', 'w', encoding='utf-8') as outfile: 
+with open('/tmp/fb_posts.json', 'w', encoding='utf-8') as outfile:
 	json.dump(posts, outfile, ensure_ascii=False, indent=3)	
 
 #events
 events = graph.get_connections(id='me', connection_name='events', fields='name,description,place,rsvp_status')
 
 #writing event data into json-File
-with open('events.json', 'w', encoding='utf-8') as outfile: 
+with open('/tmp/fb_events.json', 'w', encoding='utf-8') as outfile:
 	json.dump(events, outfile, ensure_ascii=False, indent=3)
 
 #groups
 groups = graph.get_connections(id='me', connection_name='groups', fields='name,description')
 
 #writing group data into json-File 
-with open('groups.json', 'w', encoding='utf-8') as outfile: 
+with open('/tmp/fb_groups.json', 'w', encoding='utf-8') as outfile:
 	json.dump(groups, outfile, ensure_ascii=False, indent=3)
