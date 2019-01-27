89770a27b91f0ed552d849d612d4e75ac859372b
Nicholas K <nicholaskappel@prezap.de>
2019-01-28 00:08:03 +0100

    updated docker setup

--- setup.sh
+++ setup.sh
@@ -2,9 +2,14 @@
 docker-compose build
 docker-compose up -d
 sleep 20 # give mysql container time to spin up db
-docker exec -it reddit-recommender_reddit-mysql_1 /bin/bash -c \
+docker exec -it reddit-recommender-mysql /bin/bash -c \
 'mysql -u root --password=password -e '\''CREATE USER "root"@"%" IDENTIFIED BY "password"; GRANT ALL PRIVILEGES ON *.* TO "root"@"%" WITH GRANT OPTION;'\'''
-docker exec -it reddit-recommender_reddit-mysql_1 /bin/bash -c \
-'mysql -u root --password=password < /tmp/sqlkomplett.sql'
+docker exec -it reddit-recommender-mysql /bin/bash -c \
+'mysql -u root -ppassword < /tmp/sqlkomplett.sql'
+docker exec -it reddit-recommender-mysql /bin/bash -c \
+'mysql -u root -ppassword reddit_recommender < /tmp/subredditsBigger5.sql'
+docker stop reddit-recommender-flask
+docker stop reddit-recommender-pdi
+docker stop reddit-recommender-mysql
 
 echo "Done!"
